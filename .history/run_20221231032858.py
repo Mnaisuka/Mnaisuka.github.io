8fff087b57385bf4e7eb8586dde3ec59245dbc07
moxi <2567810193@qq.com>
2022-12-31 03:29:49 +0800

    up

--- .history/run_20221231032858.py
+++ .history/run_20221231032858.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen, Request
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
-ret = Request("https://yande.re/pool", headers=headers)
+ret = Request("https://book.sfacg.com/signin/", headers=headers)
 res = urlopen(ret)
 html = res.read().decode('utf-8')
 file = open(f'./debug.html', mode='wb+')
