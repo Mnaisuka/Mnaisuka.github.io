74fb04f7873073613d79f659e96397ed239d72ec
moxi <2567810193@qq.com>
2022-12-31 04:36:41 +0800

    up

--- .history/run_20221231021505.py
+++
@@ -1,3 +0,0 @@
-file = open(f'./debug.text', mode='w+')
-file.write(f'hi,world')
-file.close()
--- .history/run_20221231033912.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen, Request
-ret = Request("https://yande.re/post?tags=wallpaper+rating%3Asafe+score%3A%3E%3D20")
-res = urlopen(ret)
-html = res.read().decode('utf-8')
-file = open(f'./debug.html', mode='wb+')
-file.write(bytes(html,'utf-8'))
-file.close()
