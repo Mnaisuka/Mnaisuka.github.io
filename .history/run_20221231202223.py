7cfa198619ab0f931c7863f90f7ec7aa2938d293
moxi <2567810193@qq.com>
2022-12-31 23:01:10 +0800

    up

--- .history/run_20221231202223.py
+++ .history/run_20221231202223.py
@@ -1,7 +1,6 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import time
-
 """ ret = Request(
     "https://yande.re/post?tags=wallpaper+rating%3Asafe+score%3A%3E%3D20")
 res = urlopen(ret)
